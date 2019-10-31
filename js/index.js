b96e770efa9275e8ec98843e634b1600a2d796ee
idmyn <hello@davidmyno.rs>
2019-10-31 17:02:01 +0000

    give option to search for users or repos

--- js/index.js
+++ js/index.js
@@ -1,30 +1,53 @@
+const configObj = {
+  method: "GET",
+  headers: {
+    "Accept": "application/vnd.github.v3+json",
+    "User-Agent": "idmyn"
+  }
+}
+
 document.querySelector("#github-form").addEventListener("submit", e => {
   e.preventDefault()
   const query = document.querySelector("#search").value
-  searchForUser(query).then(users => {
-    const ul = prepareUserList()
-    users.forEach(user => injectUser(ul, user))
-  })
+  clearLists()
+  if (document.querySelector("#user-radio").checked) {
+    searchForUsers(query).then(users => {
+      const ul = prepareUserList()
+      users.forEach(user => injectUser(ul, user))
+    })
+  } else {
+    searchForRepos(query).then(repos => {
+      const ul = document.querySelector("#repos-list")
+      removeChildren(ul)
+      repos.forEach(repo => {
+        injectRepo(ul, repo)
+      })
+    })
+  }
 })
+
+function clearLists() {
+  const lists = document.querySelectorAll("ul")
+  lists.forEach(list => removeChildren(list))
+}
+
+function removeChildren(parent) {
+  while (parent.firstChild) {
+    // https://stackoverflow.com/a/3955238
+    parent.removeChild(parent.firstChild)
+  }
+}
 
 function prepareUserList() {
   const ul = document.querySelector("#user-list")
-  removeChildren(ul)
   const li = document.createElement("li")
   li.innerHTML = "<header><strong>users</strong></header>"
   ul.append(li)
   return ul
 }
 
-function searchForUser(query) {
+function searchForUsers(query) {
   const URI = `https://api.github.com/search/users?q=${query}`
-  const configObj = {
-    method: "GET",
-    headers: {
-      "Accept": "application/vnd.github.v3+json",
-      "User-Agent": "idmyn"
-    }
-  }
   return fetch(URI, configObj)
     .then(response => response.json())
     .then(json => json.items)
@@ -35,14 +58,14 @@ function injectUser(ul, user) {
   userLi.textContent = user.login
   userLi.addEventListener("click", e => {
     lookupUserRepos(e, user.repos_url).then(repos => {
-      const ul = prepareRepoList(user)
+      const ul = prepareUserRepoList(user)
       repos.forEach(repo => injectRepo(ul, repo))
     })
   })
   ul.append(userLi)
 }
 
-function prepareRepoList(user) {
+function prepareUserRepoList(user) {
   const ul = document.querySelector("#repos-list")
   removeChildren(ul)
   const li = document.createElement("li")
@@ -52,25 +75,18 @@ function prepareRepoList(user) {
 }
 
 function lookupUserRepos(e, URI) {
-  const configObj = {
-    method: "GET",
-    headers: {
-      "Accept": "application/vnd.github.v3+json",
-      "User-Agent": "idmyn"
-    }
-  }
   return fetch(URI, configObj).then(response => response.json())
-}
-
-function removeChildren(parent) {
-  while (parent.firstChild) {
-    // https://stackoverflow.com/a/3955238
-    parent.removeChild(parent.firstChild)
-  }
 }
 
 function injectRepo(ul, repo) {
   const li = document.createElement("li")
   li.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>`
   ul.append(li)
+}
+
+function searchForRepos(query) {
+  const URI = `https://api.github.com/search/repositories?q=${query}`
+  return fetch(URI, configObj)
+    .then(response => response.json())
+    .then(json => json.items)
 }
