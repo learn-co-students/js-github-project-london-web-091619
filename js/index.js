73e50edbfe00459eb526b8beff116bbbb34e4f92
skjwise <skjwise@hotmail.com>
2019-11-27 09:24:08 +0000

    Done.

---
+++ js/index.js
@@ -0,0 +1,119 @@
+// //APIs
+// const baseURL = 'https://api.github.com/search/users?q=octocat';
+// const octocatRepoURL = 'https://api.github.com/users/octocat/repos';
+
+// //consts - variables
+// const userList = document.querySelector('#user-list')
+// const repoList = document.querySelector('#repos-list')
+// const form = document.querySelector('#github-form')
+// const submitBtn = document.querySelector('input[type="submit"]')
+
+// //functions 
+// const searchForm = (event) => {
+//     event.preventDefault()
+//     const searchInput = form.querySelector('#search')
+//     let val
+//     fetchUser(searchInput)
+//     .then(users => renderUsers(users))
+// }
+
+// const fetchUser = () => {
+//     return fetch(baseURL)
+// }
+
+
+
+//One way to complete this practice lab! 
+
+document.addEventListener('DOMContentLoaded',function(){
+    const form = document.querySelector("#github-form")
+    const submitButton = form.querySelector('input[type="submit"]')
+    
+    submitButton.addEventListener('click', search)
+})
+
+function search(e){
+    e.preventDefault()
+    const form = document.querySelector("#github-form")
+    const searchInput = form.querySelector('#search')
+    let val 
+    fetchUser(searchInput)
+        .then(function(users){
+            renderUsers(users)
+        })
+}
+
+function fetchUser(searchInput){
+    return fetch(`https://api.github.com/search/users?q=${searchInput.value}`)
+        .then(function(response){
+            return response.json()
+        })
+}
+
+function renderUsers(users){
+    usersList = document.querySelector('#user-list')
+    usersList.innerHTML = ""
+    total = document.createElement("h1")
+    total.innerText = `We found ${users.total_count} users.`
+    usersList.appendChild(total)
+
+    users.items.forEach(function(user){
+        newLi = document.createElement("li")
+
+        nameA = document.createElement("a")
+        nameA.href = user.html_url
+        nameA.innerText = user.login
+
+        newImg = document.createElement("img")
+        newImg.src = user.avatar_url
+
+        newButton = document.createElement("button")
+        newButton.id = user.repos_url
+        newButton.innerText = "Get Repos"
+        newButton.addEventListener('click', function(e){
+            e.preventDefault()
+            
+            getRepos(e.target.id)
+
+        })
+
+        newLi.appendChild(nameA)
+        newLi.appendChild(newImg)
+        newLi.appendChild(newButton)
+        usersList.appendChild(newLi)
+        
+    })
+}
+
+function getRepos(repos_url){
+    fetchRepos(repos_url)
+        .then(function(repos){
+            renderRepos(repos)
+        })
+}
+
+function fetchRepos(repos_url){
+    return fetch(repos_url)
+    .then(function(response){
+        return response.json()
+    })
+}
+
+function renderRepos(repos){
+    reposList = document.querySelector('#repos-list')
+    reposList.innerHTML = ""
+    total = document.createElement("h1")
+    total.innerText = `We found ${repos.length} repos.`
+    reposList.appendChild(total)
+
+    repos.forEach(function(repo){
+        newLi = document.createElement("li")
+
+        nameA = document.createElement("a")
+        nameA.href = repo.html_url
+        nameA.innerText = repo.name
+
+        newLi.appendChild(nameA)
+        reposList.appendChild(newLi)
+    })
+}
